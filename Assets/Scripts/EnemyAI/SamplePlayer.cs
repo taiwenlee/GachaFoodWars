3302cac22b15268d7be82cdcabba72a717768fb1
Tai Wen Lee <leetaiwen101@gmail.com>
2023-01-24 21:24:28 -0800

    added back missing files

---
+++ Assets/Scripts/EnemyAI/SamplePlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamplePlayer : MonoBehaviour
+{
+    [Header("Stats")]
+    public float health = 100.0f;
+    public float speed = 3.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement 
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, z);
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Player took " + damage + " damage!");
+    }
+}
