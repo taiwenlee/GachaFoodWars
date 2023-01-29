8e59cfbe63810213406c0b494f6eeb7433248520
pli60 <pli60@ucsc.edu>
2023-01-28 23:29:12 -0800

    Working gacha system

--- Assets/Scripts/Gacha.cs
+++ Assets/Scripts/Gacha.cs
@@ -8,10 +8,11 @@ public class Gacha : MonoBehaviour
     public List<GameObject> weapons;
     // total weight of each rarity
     [SerializeField]
-    public int[] table = {600, 300, 100};
+    public int[] table = {500, 300, 160, 40};
 
     public int totalWeight;
     public int randomNumber;
+
     private void Start()
     {
         // calculate total weight of loot table
@@ -19,23 +20,29 @@ public class Gacha : MonoBehaviour
         {
             totalWeight += item;
         }
+    }
 
-        // generate random number
-        randomNumber = Random.Range(0, totalWeight);
-
-        // compare random number to loot table weight
-        for(int i = 0; i < table.Length; i++)
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            // compare random number to the first weight in loot table, if smaller give first item
-            if(randomNumber <= table[i])
+            print("space key was pressed");
+            // generate random number
+            randomNumber = Random.Range(0, totalWeight);
+            // compare random number to loot table weight
+            for(int i = 0; i < table.Length; i++)
             {
-                weapons[i].SetActive(true);
-                Debug.Log("Award: " + table[i]);
-                return;
-            }else
-            {
-                // if random number is bigger than previous weight, subtract it with weight and compare the product to next weight in table
-                randomNumber -= table[i];
+                // compare random number to the [i] weight in loot table, if smaller give [i] item
+                if(randomNumber <= table[i])
+                {
+                    weapons[i].SetActive(true);
+                    Debug.Log("Award: " + table[i]);
+                    return;
+                }else
+                {
+                    // if random number is bigger than previous weight, subtract it with weight and compare the product to next weight in table
+                    randomNumber -= table[i];
+                }
             }
         }
     }
