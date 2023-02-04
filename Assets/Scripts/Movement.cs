5576a6b5b4c7978b6e186c2eb9429644825a088f
nlaureta <103101101+codegames123@users.noreply.github.com>
2023-02-03 16:45:20 -0800

    cursor rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,7 +11,7 @@ public class Movement : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float speed = 5f;
     [SerializeField] private float gravity = -9.81f;
-    //[SerializeField] private float deadzone = 0.1f;
+    [SerializeField] private float deadzone = 0.1f;
     [SerializeField] private float smoothRotate= 2000f;
     //private int playerSpeed = 10;
     //private int playerRotation = -5;
@@ -74,34 +74,34 @@ public class Movement : MonoBehaviour
 
 
         // Uncomment for WASD rotation
-        Vector3 move = new Vector3(movement.x,0,movement.y);
-         if(move != Vector3.zero) {
-            Quaternion toRot = Quaternion.LookRotation(move,Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,toRot,smoothRotate*Time.deltaTime);
-         }
+        // Vector3 move = new Vector3(movement.x,0,movement.y);
+        //  if(move != Vector3.zero) {
+        //     Quaternion toRot = Quaternion.LookRotation(move,Vector3.up);
+        //     transform.rotation = Quaternion.RotateTowards(transform.rotation,toRot,smoothRotate*Time.deltaTime);
+        //  }
 
 
         //Uncomment for full mouse rotation
         //using controller
-        // if(isController) {
-        //     if(Mathf.Abs(aim.x) > deadzone || Mathf.Abs(aim.y) > deadzone) {
-        //         Vector3 playerDirection = Vector3.right * aim.x + Vector3.forward * aim.y;
-        //         if(playerDirection.sqrMagnitude > 0.0f) {
-        //             Quaternion rot = Quaternion.LookRotation(playerDirection, Vector3.up);
-        //             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, smoothRotate * Time.deltaTime);
-        //         }
-        //     }
-        // //keyboard
-        // }else {
-        //     Ray ray = Camera.main.ScreenPointToRay(aim);
-        //     Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        //     float rayDistance;
+        if(isController) {
+            if(Mathf.Abs(aim.x) > deadzone || Mathf.Abs(aim.y) > deadzone) {
+                Vector3 playerDirection = Vector3.right * aim.x + Vector3.forward * aim.y;
+                if(playerDirection.sqrMagnitude > 0.0f) {
+                    Quaternion rot = Quaternion.LookRotation(playerDirection, Vector3.up);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, smoothRotate * Time.deltaTime);
+                }
+            }
+        //keyboard
+        }else {
+            Ray ray = Camera.main.ScreenPointToRay(aim);
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+            float rayDistance;
             
-        //     if(groundPlane.Raycast(ray,out rayDistance)) {
-        //         Vector3 point = ray.GetPoint(rayDistance);
-        //         LookAt(point);
-        //     }
-        // }
+            if(groundPlane.Raycast(ray,out rayDistance)) {
+                Vector3 point = ray.GetPoint(rayDistance);
+                LookAt(point);
+            }
+        }
     }
 
     private void LookAt(Vector3 lookPoint) {
