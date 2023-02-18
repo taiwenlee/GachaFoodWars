4e1b88ac0c25be2c754da2ac82be5c368a802dce
Justin Murillo <murillo_justin@yahoo.com>
2023-02-17 20:09:52 -0800

    removed unnecessary code in RestAreaManager.cs

--- Assets/LevelAssets/Scripts/RestAreaManager.cs
+++ Assets/LevelAssets/Scripts/RestAreaManager.cs
@@ -8,18 +8,9 @@ public class RestAreaManager : MonoBehaviour
     [SerializeField] Gate exitGate;
     [SerializeField] int numberOfLevels;
     [SerializeField] int numberOfRooms;
-    [SerializeField] Gacha gacha;
-    [SerializeField] GameObject gachaUI;
-    [SerializeField] TextMeshProUGUI weaponObtainedUI;
 
     public LevelStructureProto levelStructure;
     public PlayerLevelProgression plp;
-
-    private void Start()
-    {
-        gacha.GachaUI = gachaUI;
-        gacha.WeaponObtainedUI = weaponObtainedUI;
-    }
 
     // Update is called once per frame
     void Update()
