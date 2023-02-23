09669207f07af2bd76ca3caf2ce6cfa7f3ff1a82
Soiiz <kchau6@ucsc.edu>
2023-02-22 20:00:10 -0800

    UI scene transition fix

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Rest");
     }
 
     public void QuitGame(){
