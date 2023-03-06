e989feffce1fb25ece250026482e46c75de9e809
nlaureta <103101101+codegames123@users.noreply.github.com>
2023-03-06 01:16:00 -0800

    dynamic heart system
    uses a prefab to spawn x num of hearts

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -5,14 +5,32 @@ using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
+    [SerializeField] GameObject heartPrefab;
+
     public GameObject restartMenu;
     public PlayerStats playerStats;
     //private bool playerDead = false;
     //private bool showDeathUI = false;
     private float restartMenuTimer = 0.0f;
     public int health = 4;
+    GameObject heart;
     
-    [SerializeField] private Image[] numHearts;
+    //[SerializeField] private Image[] numHearts;
+
+    public void DrawHearts(int hearts, int maxHearts) {
+        foreach(Transform child in transform) { //destroy so no hearts on top of each other
+            Destroy(child.gameObject);
+        }
+
+        for(int i = 0; i < maxHearts; i++) {
+            if(i  + 1 <= hearts) {
+                heart = Instantiate(heartPrefab, transform.position, Quaternion.identity); //spawn hearts
+                heart.transform.SetParent(transform);
+            }else {
+                Destroy(heart); //destroy if damaged
+            }
+        }
+    }
 
     private void Start(){
         UpdateHealth();
@@ -46,13 +64,13 @@ public class PlayerUI : MonoBehaviour
             //Time.timeScale = 0f;
            
         }
-        for(int i = 0; i < numHearts.Length; i++) {
-            if(i < health) {
-                numHearts[i].color = Color.red;
-            } else {
-                numHearts[i].color = Color.black;
-            }
-        }
+        // for(int i = 0; i < numHearts.Length; i++) {
+        //     if(i < health) {
+        //         numHearts[i].color = Color.red;
+        //     } else {
+        //         numHearts[i].color = Color.black;
+        //     }
+        // }
     }
 
     public void SetHealth(int currentHealth)
