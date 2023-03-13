b84471fdb1f2df11dff6bc6ed3f35a3b43120923
Soiiz <kchau6@ucsc.edu>
2023-03-13 04:18:45 -0700

    ToolTip scripts updated

--- Assets/Scripts/Tool Tip/HoverTipManager.cs
+++ Assets/Scripts/Tool Tip/HoverTipManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using System;
 using UnityEngine;
 
-public class ToolTipManager : MonoBehaviour
+public class HoverTipManager : MonoBehaviour
 {
+    public TextMeshProUGUI tipText;
+    public RectTransform tipWindow;
+
+    public static Action <string, Vector2> OnMouseHover;
+    public static Action OnMouseLoseFocus;
+
+    private void OnEnable()
+    {
+        OnMouseHover += ShowTip;
+        OnMouseLoseFocus+= HideTip;
+    }
+
+    private void OnDisable()
+    {
+        OnMouseHover -= ShowTip;
+        OnMouseLoseFocus -= HideTip;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideTip();
+    }
+    
+    private void ShowTip(string tip, Vector2 mousePos)
+    {
+        tipText.text = tip;
+        tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 200 ? 200 : tipText.preferredWidth, tipText.preferredHeight);
+
+        tipWindow.gameObject.SetActive(true);
+        tipWindow.transform.position = new Vector2(mousePos.x + tipWindow.sizeDelta.x * 2, mousePos.y);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideTip()
     {
-        
+        tipText.text = default;
+        tipWindow.gameObject.SetActive(false);
     }
 }
